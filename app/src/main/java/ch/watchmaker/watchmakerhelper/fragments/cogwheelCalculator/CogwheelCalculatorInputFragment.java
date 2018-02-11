e94cb34abce2ca5f2d44e5320415937d66f4f97e
Silvan Spycher <silvan.spycher@gmail.com>
2018-02-11 11:51:35 +0100

    adds failsave stuff

--- app/src/main/java/ch/watchmaker/watchmakerhelper/fragments/cogwheelCalculator/CogwheelCalculatorInputFragment.java
+++ app/src/main/java/ch/watchmaker/watchmakerhelper/fragments/cogwheelCalculator/CogwheelCalculatorInputFragment.java
@@ -2,10 +2,14 @@ package ch.watchmaker.watchmakerhelper.fragments.cogwheelCalculator;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,7 +36,7 @@ public class CogwheelCalculatorInputFragment extends BaseFragment implements Cog
     @BindView(R.id.fci_edt_minteeth)EditText fci_edt_minteeth;
     @BindView(R.id.fci_edt_maxteeth)EditText fci_edt_maxteeth;
     @BindView(R.id.fci_edt_nrcogs)EditText fci_edt_nrcogs;
-
+    @BindView(R.id.fci_btn_calculate)Button fci_btn_calculate;
 
 
 
@@ -45,9 +49,24 @@ public class CogwheelCalculatorInputFragment extends BaseFragment implements Cog
             presenter = new CogwheelCalculatorInputFragmentPresenterImpl(this);
         }
 
+        fci_edt_maxteeth.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    calculatePressed();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         return view;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public int getLayout() {
         return R.layout.fragment_cogwheel_input;
@@ -74,12 +93,31 @@ public class CogwheelCalculatorInputFragment extends BaseFragment implements Cog
         if (fci_edt_nrcogs.getText().toString().equals("")) {
             fci_edt_nrcogs.setText(fci_edt_nrcogs.getHint());
         }
+
+        //TODO check for wrong inputs
+
+
         double startcog = Double.parseDouble(fci_edt_startcog.getText().toString());
         double endcog = Double.parseDouble(fci_edt_endcog.getText().toString());
         double error = Double.parseDouble(fci_edt_error.getText().toString());
-        int minteeth = Integer.parseInt(fci_edt_minteeth.getText().toString());
-        int maxteeth = Integer.parseInt(fci_edt_maxteeth.getText().toString());
-        int nrcogs = Integer.parseInt(fci_edt_nrcogs.getText().toString());
+        int minteeth;
+        int maxteeth;
+        int nrcogs;
+        try {
+            minteeth = Integer.parseInt(fci_edt_minteeth.getText().toString());
+        } catch (Exception e) {
+            minteeth = 6;
+        }
+        try {
+            maxteeth = Integer.parseInt(fci_edt_maxteeth.getText().toString());
+        } catch (Exception e) {
+            maxteeth = 120;
+        }
+            try {
+            nrcogs = Integer.parseInt(fci_edt_nrcogs.getText().toString());
+        } catch (Exception e) {
+            nrcogs = 4;
+        }
 
         //set values if 0
         if (minteeth == 0) minteeth = 1;
